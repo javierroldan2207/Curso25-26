235ad73826f7a0cc4492574d939ad152e10e00c2
javierroldan2207 <javierroldan2207@gmail.com>
2025-09-30 10:12:44 +0200

    Subida ejercicio1 formulario

--- DWES/Formularios/Ejercicio1.php
+++ DWES/Formularios/Ejercicio1.php
@@ -3,18 +3,18 @@
 
 
 
-<form method = "GET">
+<form method = "POST">
   <div class="form-group row">
     <label class="col-4 col-form-label" for="name">Nombre</label> 
     <div class="col-8">
       <input id="name" name="name" placeholder="Introduzca su nombre" type="text" class="form-control" value ="<?php
-        if($_SERVER["REQUEST_METHOD"] === "GET" && !empty($_GET["name"] && isset($_GET["name"]))){
-            echo $_GET["name"];
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["name"]) && !empty($_POST["name"])){
+            echo $_POST["name"];
         }?>">
       <?php
       $flag = false;
-      if($_SERVER["REQUEST_METHOD"] === "GET" && !empty($_GET["name"] && isset($_GET["name"]))){
-        if($_GET["edad"] > 0 && $_GET["edad"] <= 120){
+      if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["name"]) && !empty($_POST["name"])){
+        if($_POST["edad"] > 0 && $_POST["edad"] <= 120){
             $flag = true;
             }
         }else{
@@ -26,17 +26,17 @@
   <div class="form-group row">
     <label for="edad" class="col-4 col-form-label">Edad</label> 
     <div class="col-8">
-      <input id="edad" name="edad" placeholder="Introduzca su edad" type="text" class="form-control">
+      <input id="edad" name="edad" placeholder="Introduzca su edad" type="text" class="form-control" value="<?php 
+      if($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST["edad"]) && isset($_POST["edad"])){
+            echo $_POST["edad"];
+        }?>">
       <?php 
-        if($_SERVER["REQUEST_METHOD"] === "GET" && !empty($_GET["edad"] && isset($_GET["edad"]) && is_numeric($_GET["edad"]))){
-            if($_GET["edad"] > 0 && $_GET["edad"] <= 120 && $flag){
-                echo 'Bienvenido '. $_GET["name"].", ". "tu edad es ".$_GET["edad"]; 
-            }
-            
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["edad"]) && !empty($_POST["edad"] && is_numeric($_POST["edad"]))){
+            if($_POST["edad"] > 0 && $_POST["edad"] <= 120 && $flag){
+                echo 'Bienvenido '. $_POST["name"].", ". "tu edad es ".$_POST["edad"]; 
+            }  
         }else{
-                if($_GET["edad"] <= 0 || $_GET["edad"] > 120){
-                    echo "Error: campo edad no esta en el rango."; 
-                }
+                echo "Error: campo edad no esta en el rango."; 
             }
 ?>
     </div>
